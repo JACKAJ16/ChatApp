c32f0be48ea24c1b5a369793d08a9cc05a43b246
JACKAJ16 <zhenya271@gmail.com>
2020-05-11 23:33:18 +0300

    Adding build

---
+++ precache-manifest.18926855e2b5fe8f23c12da0a9cafa48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52e37f2ec59514c5a7803b47264b5d6",
+    "url": "/ChatApp/index.html"
+  },
+  {
+    "revision": "0c68b57e27d1715f7f2c",
+    "url": "/ChatApp/static/css/main.608f29e6.chunk.css"
+  },
+  {
+    "revision": "7bb13e1192324a1ac22c",
+    "url": "/ChatApp/static/js/2.1d458a21.chunk.js"
+  },
+  {
+    "revision": "fcdc5e536d87d4615bb78a0d5fa80be3",
+    "url": "/ChatApp/static/js/2.1d458a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c68b57e27d1715f7f2c",
+    "url": "/ChatApp/static/js/main.0a893ffb.chunk.js"
+  },
+  {
+    "revision": "46ca27d92ff915c24b3e",
+    "url": "/ChatApp/static/js/runtime-main.53e758ca.js"
+  },
+  {
+    "revision": "95c9b2fe4f7847a80628b10c29f737d0",
+    "url": "/ChatApp/static/media/arrow.95c9b2fe.png"
+  },
+  {
+    "revision": "b2681e174b32978e4a5deb39855cd56e",
+    "url": "/ChatApp/static/media/background.b2681e17.jpg"
+  }
+]);
